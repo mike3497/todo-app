2af299dfaa38ea785a164c92ada5bac930f76c26
Michael Carillon <mcarillon97@gmail.com>
2025-09-01 21:34:02 -0400

    feat: use local storage

--- src/stores/todos.ts
+++ src/stores/todos.ts
@@ -1,9 +1,20 @@
 import { type Todo } from '@/models/todo';
+import { useLocalStorage } from '@vueuse/core';
 import { defineStore } from 'pinia';
-import { ref } from 'vue';
 
 export const useTodosStore = defineStore('todosStore', () => {
-  const todos = ref<Todo[]>([]);
+  const todos = useLocalStorage<Todo[]>('todos', [
+    { id: '1', title: 'Buy groceries', completed: false },
+    { id: '2', title: 'Walk the dog', completed: true },
+    { id: '3', title: 'Read a book', completed: true },
+    { id: '4', title: 'Finish homework', completed: false },
+    { id: '5', title: 'Call mom', completed: false },
+    { id: '6', title: 'Clean the kitchen', completed: false },
+    { id: '7', title: 'Pay bills', completed: false },
+    { id: '8', title: 'Exercise', completed: true },
+    { id: '9', title: 'Plan weekend trip', completed: false },
+    { id: '10', title: 'Reply to emails', completed: true },
+  ]);
 
   const addTodo = (todo: Todo) => {
     todos.value.unshift(todo);

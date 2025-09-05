44331c04ae549458e90d60bf1065e82ad33d8cf9
Michael Carillon <mcarillon97@gmail.com>
2025-09-04 22:13:40 -0400

    feat: add footer

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <template>
-  <div class="w-full h-svh flex items-center justify-center p-6">
+  <div class="w-full h-svh flex flex-col items-center justify-center gap-1 p-6">
     <div class="bg-white w-lg rounded-lg p-6 flex flex-col gap-4">
       <h1 class="font-bold text-3xl text-center">Todo App</h1>
       <TodoForm />
       <TodoList />
     </div>
+    <footer class="text-sm text-white opacity-75">
+      Created by
+      <a class="underline" href="https://michaelcarillon.com/" target="_blank">Michael Carillon</a>
+    </footer>
   </div>
   <ConfirmationDialog />
 </template>
